632bc9bbd96e11fce3a77b35b36d4cfff0b33f71
github username <github email address>
2016-07-06 15:42:11 -0400

    lab complete

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -25,45 +25,55 @@ let b = 32
 ### 1. Print the result of a greater than or equal to b
 */
 // write your code here
+print(a >= b)
 
 
 /*: Question 2
 ### 2. Print the result of a modulo b is equal to zero
 */
 // write your code here
-
+print(a%b == 0)
 
 /*: Question 3
 ### 3. Print the result of y times b less than or equal to a
 */
 // write your code here
-
+print(y*b <= a)
 
 /*: Question 4
 ### 4. Print the inverse of a greater than or equal to b
 */
 // write your code here
-
+print(!(a >= b))
 
 
 /*: Question 5
 ### 5. Print "true" if a modulo b is equal to zero
 */
 // write your code here
-
+if a%b == 0 {
+    print("true")
+}
 
 /*: Question 6
 ### 6. Print "true" if a divided by b is greater than x
 */
 // write your code here
-
+if Double(a/b) > x {
+    print("true")
+}
 
 
 /*: Question 7
 ### 7. Print "true" if y divided by x is greater than three, otherwise print false
 */
 // write your code here
-
+if Double(y)/x > 3 {
+    print("true")
+}
+else {
+    print("false")
+}
 
 
 
@@ -72,7 +82,9 @@ let b = 32
 */
 // write your code here
 
-
+if Double(y) > x && a/b > 9 {
+    print("true")
+}
 
 
 /*: Question 9
@@ -80,7 +92,14 @@ let b = 32
 */
 // write your code here
 
-
+func isGreater(num1 : Int, num2: Int) -> Bool{
+    if num1 > num2 {
+        return true
+    }
+    else {
+        return false
+    }
+}
 
 
 
@@ -90,15 +109,34 @@ let b = 32
 */
 // write your code here
 
-
+func isForceWith(str: String) -> Bool {
+    let peopleOfForce = ["Luke", "Leia", "Anakin", "Obi Wan", "Yoda", "Vader"]
+    for people in peopleOfForce {
+        if str == people {
+            return true
+        }
+    }
+    return false
+}
 
 /*: Question 11
 ### 11. Create a function where the two arguments represent different bank account (one of those bank accounts is yours). What type should these arguments be if we are to then perform some math operations on them? Setup a conditional that will add 10 to your funds and minus 10 from the other funds (bank account) if the other account won't go negative if we were to take 10 dollars from it.
 */
 // write your code here
 
-
-
+func bankAccounts(myBankAccount: Double, otherBankAccount: Double) -> [Double]{
+    var othersNewBalance : Double = 0.0
+    var myNewBalance : Double = 0.0
+    if otherBankAccount - Double(10) >= 0 {
+        othersNewBalance = otherBankAccount - Double(10)
+        myNewBalance = myBankAccount + Double(10)
+    }
+    else {
+        othersNewBalance = otherBankAccount
+        myNewBalance = myBankAccount
+    }
+    return [myNewBalance, othersNewBalance]
+}
 
 
 /*:
